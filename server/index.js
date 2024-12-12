f4de83e266bd9de2061fa5fdaff2db7fb7ff5da6
raycastillo3 <60750068+raycastillo3@users.noreply.github.com>
2024-12-12 16:04:23 -0500

    added concurrently command

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 const express = require('express'); 
+const app = express(); 
+const PORT = 5050; 
 
+app.get("/", (req, res) =>{
+    res.send("Welcome to API :)")
+})
+
+app.listen(PORT, () => {
+    console.info(`Server listening on PORT: ${PORT}`)
+})
